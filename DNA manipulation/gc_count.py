8d9fbb565cab172abd87b39b3205c2e143bc02dc
Sherin-ann <58263511+Sherin-ann@users.noreply.github.com>
2020-05-13 16:12:54 +0530

    Create gc_count.py

---
+++ DNA manipulation/gc_count.py
@@ -0,0 +1,15 @@
+#usage python3 gc_count.py
+
+#function definition
+def GCCount(s):
+    s = s.upper()
+    count_G = s.count('G') 
+    count_C = s.count('C')
+    GC = ((count_G + count_C) / float(len(s))) * 100
+    print("\nGC Content is: %.4f" %GC,"%")
+
+#Input sequence
+s = input("Enter your sequence: ")
+
+#call function
+GCCount(s)

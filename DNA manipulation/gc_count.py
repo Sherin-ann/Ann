7b83a823d6f0c79ab512ac13505da4c775425b49
Sherin-ann <58263511+Sherin-ann@users.noreply.github.com>
2020-05-13 16:29:02 +0530

    Update gc_count.py

--- DNA manipulation/gc_count.py
+++ DNA manipulation/gc_count.py
@@ -5,6 +5,7 @@ def GCCount(s):
     s = s.upper()
     count_G = s.count('G') 
     count_C = s.count('C')
+    #count_GC = s.count('CG')   CG dinucleotide count
     GC = ((count_G + count_C) / float(len(s))) * 100
     print("\nGC Content is: %.4f" %GC,"%")
 
